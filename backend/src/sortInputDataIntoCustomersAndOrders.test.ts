a8482f716e65f718a0ec63812c6b58d8988dbc04
David Johnston <djohnston@cydarm.com>
2023-09-06 18:16:13 +1000

    Implement the data conversion function.

--- backend/src/sortInputDataIntoCustomersAndOrders.test.ts
+++ backend/src/sortInputDataIntoCustomersAndOrders.test.ts
@@ -1,7 +1,97 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test, it } from '@jest/globals';
+import { sortDataIntoCustomersAndOrders } from './sortInputDataIntoCustomersAndOrders';
 
-describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(1 + 1).toBe(2)
+
+describe(sortDataIntoCustomersAndOrders, () => {
+    it('Converts the data properly', () => {
+
+        const inputData = [{
+            "id": 1,
+            "vendor": "acme",
+            "date": "03/03/2017",
+            "customer": {
+                "id": "8baa6dea-cc70-4748-9b27-b174e70e4b66",
+                "name": "Lezlie Stuther",
+                "address": "19045 Lawn Court"
+            },
+            "order": {
+                "hat": {
+                    "quantity": 14,
+                    "price": 8
+                },
+                "cake": {
+                    "quantity": 9,
+                    "price": 3
+                },
+                "ice": {
+                    "quantity": 10,
+                    "price": 5
+                },
+                "candy": {
+                    "quantity": 6,
+                    "price": 8
+                }
+            }
+        }];
+
+        const outputData = {
+            "customers": [
+                {
+                    "id": "8baa6dea-cc70-4748-9b27-b174e70e4b66",
+                    "name": "Lezlie Stuther",
+                    "address": "19045 Lawn Court"
+                }
+            ],
+            "orders": [
+                {
+                    "id": 1,
+                    "vendor": "acme",
+                    "date": "03/03/2017",
+                    "customer": "8baa6dea-cc70-4748-9b27-b174e70e4b66",
+                    "order": [
+                        {
+                            "item": "hat",
+                            "quantity": 14,
+                            "price": 8,
+                            "revenue": 112
+                        },
+                        {
+                            "item": "cake",
+                            "quantity": 9,
+                            "price": 3,
+                            "revenue": 27
+                        },
+                        {
+                            "item": "ice",
+                            "quantity": 10,
+                            "price": 5,
+                            "revenue": 50
+                        },
+                        {
+                            "item": "candy",
+                            "quantity": 6,
+                            "price": 8,
+                            "revenue": 48
+                        }
+                    ]
+                }
+            ]
+        }
+
+        const result = sortDataIntoCustomersAndOrders(inputData)
+        expect(result.customers.length).toBe(outputData.customers.length);
+        expect(result.orders.length).toBe(outputData.orders.length);
+        expect(result).toEqual(outputData);
     });
+
+    it.skip('If the same users appears multiple times in the input data, they will only appear once in the output data', () => {
+        // In the interests of time, I'm not going to write for this functionality. 
+        // This is something I'd flag in development. 
+        // For example, if the user _does_ appear multiple times, 
+        // But with different names, addresses
+        // Then which name, address should be considered the canonical one? 
+
+        // As it is, the input data does not appear to contain duplicates
+    });
+
 });
--- backend/src/sortInputDataIntoCustomersAndOrders.ts
+++ backend/src/sortInputDataIntoCustomersAndOrders.ts
@@ -28,10 +28,14 @@ type InputData = {
         name: string;
         address: string;
     }
-    order: Record<string, {
+
+    // I need to look into why partial is necessary 
+    // https://www.typescriptlang.org/play?#code/MYewdgzgLgBAJgQygmBeGBtAUDXMDeOexCAXDAIwA0MRxuARuQEx0wC+Nbh9eZlbdlgC6WMVACeABwCmMAGoIANgFc56AEozQAJzgAeaDoCWYAOY0wKgLYMZOgHwBuWmIBmKsMCjHwMBMDAMlJQEIqqMgAUAG7KahDkAII6OggS+uFqDgCUBGJCYgFBIWFxUYjI2UA
+    order: Partial<Record<string, {
         quantity: number;
         price: number;
-    }>;
+    }>>
+
 }
 
 
@@ -39,5 +43,41 @@ export function sortDataIntoCustomersAndOrders(inputData: Array<InputData>): {
     orders: Array<Order>,
     customers: Array<Customer>
 } {
+
+    const customers = [] as Array<Customer>;
+    const orders = [] as Array<Order>;
+
+    inputData.forEach((v) => {
+        customers.push(v.customer)
+
+        const orderItems = Object.entries(v.order).map((v) => {
+            const [key, value] = v;
+
+            if (!value) {
+                throw new Error("Encountered a nullish value");
+            }
+
+            return {
+                item: key,
+                quantity: value.quantity,
+                price: value.price,
+                revenue: value.quantity * value.price
+            };
+        });
+
+        orders.push({
+            id: v.id,
+            vendor: v.vendor,
+            date: v.date,
+            customer: v.customer.id,
+            order: orderItems
+        })
+    })
+
+
+    return {
+        orders,
+        customers
+    }
 
 }
