078310fd55ff185095dad5a688f53160b9c9f15b
David Johnston <david@blacksheepcode.com>
2023-09-07 18:03:34 +1000

    Update server call to use new population technique.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,31 @@
 import { getFakeDb } from "./db";
 import { createServer } from "./server";
+import { sortDataIntoCustomersAndOrders } from "./sortInputDataIntoCustomersAndOrders";
 const port = process.env.PORT ?? 3001;
+const pathToInitialData = process.env.PATH_TO_INITIAL_DATA ?? "data/data.json";
 
 
 async function main() {
-    const fakeDb = await getFakeDb();
-    const server = createServer(fakeDb);
 
+    console.info("Starting server...")
+
+    console.info("Getting database...");
+    const fakeDb = await getFakeDb();
+    console.info(`Got database. Populating database from "${pathToInitialData}"...`)
+    await sortDataIntoCustomersAndOrders(pathToInitialData, fakeDb);
+    console.info("Database populated.")
+
+    const server = createServer(fakeDb);
     server.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
 }
 
 
-main();
+try {
+    main();
+} catch (err) {
+    console.error("Something went wrong starting the server")
+    // However you want to handle this. 
+    throw err;
+}
